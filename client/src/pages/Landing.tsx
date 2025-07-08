ac8b0f6de2962a69587c5f49bd90e90402e0559e
brunoaaveloso <brunoaaveloso@gmail.com>
2025-07-08 02:59:59 -0300

    Ajuste final para deploy no Vercel

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -6,7 +6,7 @@ import publicFetch from "../utils/publicFetch";
 
 export const loader = async () => {
   try {
-    const res = await publicFetch("/api/v1/mutiroes/todos");
+    const res = await publicFetch("/mutiroes/todos");
     return { mutiroes: res.data.mutiroes };
   } catch (err) {
     console.error("Erro ao carregar mutirões:", err);

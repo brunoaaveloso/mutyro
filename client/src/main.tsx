e65239444cd80b4b9edfa0429e82ae9e30b35c59
brunoaaveloso <brunoaaveloso@gmail.com>
2025-07-08 12:43:31 -0300

    Adição de axios.defaults.withCredentials = true; na main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
+import App from "./App.tsx";
+import axios from "axios";
 
-import App from './App.tsx'
-
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
